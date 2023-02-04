9c95355722c34a76d933ed90ba6c4fffd5d1ac62
Abhi <abhishek802314@gmail.com>
2023-02-04 18:11:12 +0530

    added pg database

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -81,11 +81,11 @@ from secrets_1 import PGDATABASE, PGHOST,PGPASSWORD,PGPORT,PGUSER
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get(PGDATABASE),
-        'USER': os.environ.get(PGUSER),
-        'PASSWORD':os.environ.get(PGPASSWORD),
-        'HOST':os.environ.get(PGHOST),
-        'PORT':os.environ.get(PGPORT)
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD':os.environ.get('PGPASSWORD'),
+        'HOST':os.environ.get('PGHOST'),
+        'PORT':os.environ.get('PGPORT')
     }
 }
 
