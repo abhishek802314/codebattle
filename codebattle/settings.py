948b68fb3ce5be874146f63a19b111bafb0c95fc
Abhi <abhishek802314@gmail.com>
2023-02-04 14:51:28 +0530

    -

--- codebattle/settings.py
+++ codebattle/settings.py
@@ -132,4 +132,4 @@ STATIC_ROOT = BASE_DIR /'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ORIGIN_ALLOW_ALL:True
