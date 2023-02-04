918cfc70931cbdec3effd40b264dcd2fee264e5b
abhishek802314 <95101159+abhishek802314@users.noreply.github.com>
2023-02-04 18:03:07 +0530

    Delete secrets_1.py

--- secrets_1.py
+++
@@ -1,7 +0,0 @@
-DATABASE_URL = 'postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}'
-PGDATABASE = 'railway'
-PGHOST = 'containers-us-west-103.railway.app'
-PGPASSWORD = 'TxsJu0pFxyRuIXme4o1u'
-PGPORT = '7178'
-PGUSER = 'postgres'
-
